4a5849f61b7a0fca1bb85ce4a722361735c73caf
k-vasanth09072002 <vasanthvasath74@gmail.com>
2022-12-22 16:53:39 +0530

    object-value-promt-user

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 
 
-let use={
-    name:prompt("enter value"),
-    age:parseInt(prompt("age")),
-    course:prompt("course"),
+// modal 1// epdi nalum pannalam 2method
+
+// let use={
+//     name:prompt("enter value"),
+//     age:parseInt(prompt("age")),
+//     course:prompt("course"),
+    
+// }
+// console.log(use);
+
+
+//modal 2//
+
+let val={
+    name:"",
+    age:"",
+    course:"",
     
 }
-console.log(use);
+val.name=prompt("name");
+val.age=prompt("age");
+val.course=prompt("course");
+console.log(val);
